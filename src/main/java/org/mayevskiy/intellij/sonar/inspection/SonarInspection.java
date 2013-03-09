ce492b4eaa5957412483b92d5f00027b5e844446
Michail Plushnikov <michail@plushnikov.de>
2013-03-09 22:28:15 +0100

    - select proper element - cleanup code

--- src/main/java/org/mayevskiy/intellij/sonar/inspection/SonarInspection.java
+++ src/main/java/org/mayevskiy/intellij/sonar/inspection/SonarInspection.java
@@ -3,16 +3,11 @@ package org.mayevskiy.intellij.sonar.inspection;
 import com.intellij.codeHighlighting.HighlightDisplayLevel;
 import com.intellij.codeInspection.*;
 import com.intellij.openapi.editor.Document;
-import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.fileTypes.StdFileTypes;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiElementVisitor;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiJavaFile;
-import com.intellij.psi.util.PsiUtil;
+import com.intellij.psi.*;
 import org.apache.commons.lang.StringUtils;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
@@ -63,18 +58,6 @@ public class SonarInspection extends BaseJavaLocalInspectionTool {
 
     private Map<String, Collection<Violation>> violationsMap;
 
-
-    // resource key my:project:de.plushnikov.TestKlasse
-//            filepath E:/Projekte/Idea/sonar/src/de/plushnikov/TestKlasse.java
-//            base dir E:/Projekte/Idea/sonar
-    public static String convertResourceKeyToFilePath(String resourceKey) {
-        String convertedKey = null;
-        if (null != resourceKey) {
-            convertedKey = resourceKey.substring(resourceKey.lastIndexOf((":")) + 1).replaceAll("\\.", "/");
-        }
-        return convertedKey;
-    }
-
     public String convertPsiFileToSonarKey(@NotNull PsiFile file, @NotNull String sonarProjectKey) {
         final StringBuilder result = new StringBuilder();
         result.append(sonarProjectKey).append(":");
@@ -110,10 +93,32 @@ public class SonarInspection extends BaseJavaLocalInspectionTool {
     @Nullable
     @Override
     public ProblemDescriptor[] checkFile(@NotNull PsiFile file, @NotNull InspectionManager manager, boolean isOnTheFly) {
+        createViolationsMap(manager.getProject());
 
+        final Collection<ProblemDescriptor> result = new ArrayList<>();
+
+        String resourceKey = convertPsiFileToSonarKey(file, getSonarSettingsBean(manager.getProject()).resource);
+        Collection<Violation> violations = violationsMap.get(resourceKey);
+        if (null != violations) {
+            for (Violation violation : violations) {
+                PsiElement element = getElementAtLine(file, violation.getLine() - 1);
+                if (null != element) {
+                    result.add(manager.createProblemDescriptor(
+                            element,
+                            violation.getMessage(),
+                            ProblemHighlightType.GENERIC_ERROR_OR_WARNING,
+                            null,
+                            isOnTheFly));
+                }
+            }
+        }
+        return result.toArray(new ProblemDescriptor[result.size()]);
+    }
+
+    private void createViolationsMap(Project project) {
         if (null == violationsMap) {
             violationsMap = new HashMap<>();
-            SonarSettingsBean sonarSettingsBean = getSonarSettingsBean(manager.getProject());
+            SonarSettingsBean sonarSettingsBean = getSonarSettingsBean(project);
             List<Violation> violations = new SonarService().getViolations(sonarSettingsBean);
             for (Violation violation : violations) {
                 String resourceKey = violation.getResourceKey();
@@ -127,48 +132,7 @@ public class SonarInspection extends BaseJavaLocalInspectionTool {
                 violation.getResourceKey();
                 violation.getResourceQualifier();
             }
-            // resource key my:project:de.plushnikov.TestKlasse
-//            filepath E:/Projekte/Idea/sonar/src/de/plushnikov/TestKlasse.java
-//            base dir E:/Projekte/Idea/sonar
         }
-
-
-//        VirtualFile[] sourceRoots = ModuleRootManager.getInstance(ModuleUtil.findModuleForPsiElement(file)).getSourceRoots();
-//        ProjectRootManager.getInstance(file.getProject()).getFileIndex().getSourceRootForFile(file.getVirtualFile())
-//        VirtualFile[] sourceRoots = ModuleRootManager.getInstance(module).getSourceRoots();
-//        CreateClassUtil.obtainDirectoryRootForPackage(module, "my.package.name");
-//        ModuleRootManager rootManager = ModuleRootManager.getInstance(ModuleUtil.findModuleForPsiElement(file));
-//        VirtualFile[] contentRoots = rootManager.getContentRoots();
-
-
-        final Collection<ProblemDescriptor> result = new ArrayList<>();
-        final FileDocumentManager documentManager = FileDocumentManager.getInstance();
-        Document document = documentManager.getDocument(file.getVirtualFile());
-
-        String resourceKey = convertPsiFileToSonarKey(file, getSonarSettingsBean(manager.getProject()).resource);
-        Collection<Violation> violations = violationsMap.get(resourceKey);
-        if (null != violations) {
-            for (Violation violation : violations) {
-                if (null != document && violation.getLine() < document.getLineCount()) {
-                    final int startOffset = document.getLineStartOffset(violation.getLine());
-                    PsiElement element = PsiUtil.getElementAtOffset(file, startOffset);
-
-//                final String message = violation.getRuleName();
-                    String message = violation.getMessage();
-                    ProblemDescriptor descriptor = manager.createProblemDescriptor(
-                            element,
-                            message,
-                            ProblemHighlightType.GENERIC_ERROR_OR_WARNING,
-                            null,
-                            isOnTheFly);
-
-                    result.add(descriptor);
-                }
-            }
-        }
-
-
-        return result.toArray(new ProblemDescriptor[result.size()]);
     }
 
     @NotNull
@@ -183,50 +147,40 @@ public class SonarInspection extends BaseJavaLocalInspectionTool {
         return sonarProjectComponent.getState();
     }
 
+    @Nullable
+    private PsiElement getElementAtLine(@NotNull PsiFile file, int line) {
+        PsiElement element = null;
+        final Document document = PsiDocumentManager.getInstance(file.getProject()).getDocument(file);
+        if (null != document) {
+            int offset = document.getLineStartOffset(line);
+            element = file.getViewProvider().findElementAt(offset);
+            if (element != null && document.getLineNumber(element.getTextOffset()) != line) {
+                element = element.getNextSibling();
+            }
+        }
+        return element;
+    }
+
     @NotNull
     @Override
     public PsiElementVisitor buildVisitor(@NotNull final ProblemsHolder holder, final boolean isOnTheFly) {
         return new PsiElementVisitor() {
             @Override
             public void visitFile(PsiFile file) {
-                if (null == violationsMap) {
-                    violationsMap = new HashMap<>();
-                    SonarSettingsBean sonarSettingsBean = getSonarSettingsBean(file.getProject());
-                    List<Violation> violations = new SonarService().getViolations(sonarSettingsBean);
-                    for (Violation violation : violations) {
-                        String resourceKey = violation.getResourceKey();
-                        Collection<Violation> entry = violationsMap.get(resourceKey);
-                        if (null == entry) {
-                            entry = new ArrayList<>();
-                            violationsMap.put(resourceKey, entry);
-                        }
-                        entry.add(violation);
-
-                        violation.getResourceKey();
-                        violation.getResourceQualifier();
-                    }
-                }
-
-                final FileDocumentManager documentManager = FileDocumentManager.getInstance();
-                Document document = documentManager.getDocument(file.getVirtualFile());
+                createViolationsMap(file.getProject());
 
                 String resourceKey = convertPsiFileToSonarKey(file, getSonarSettingsBean(file.getProject()).resource);
                 Collection<Violation> violations = violationsMap.get(resourceKey);
                 if (violations != null) {
                     for (Violation violation : violations) {
-                        if (null != document && violation.getLine() <= document.getLineCount()) {
-                            final int lineStartOffset = document.getLineStartOffset(violation.getLine() - 1);
-                            final int lineEndOffset = document.getLineEndOffset(violation.getLine() - 1);
-                            PsiElement elementStart = PsiUtil.getElementAtOffset(file, lineStartOffset);
-                            PsiElement elementEnd = PsiUtil.getElementAtOffset(file, lineEndOffset);
-
+                        PsiElement element = getElementAtLine(file, violation.getLine() - 1);
+                        if (null != element) {
                             ProblemDescriptor descriptor = holder.getManager().createProblemDescriptor(
-                                    elementStart,
-                                    elementEnd,
+                                    element,
                                     violation.getMessage(),
                                     ProblemHighlightType.GENERIC_ERROR_OR_WARNING,
+                                    null,
                                     isOnTheFly);
-
                             holder.registerProblem(descriptor);
                         }
                     }
