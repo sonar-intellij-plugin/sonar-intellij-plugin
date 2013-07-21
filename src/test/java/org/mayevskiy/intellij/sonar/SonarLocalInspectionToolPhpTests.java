f267a5dd6485708954ac0d6d49c85bdf564ea996
Michail Plushnikov <michail@plushnikov.de>
2013-07-21 18:02:20 +0200

    DRY configure log4j

--- src/test/java/org/mayevskiy/intellij/sonar/SonarLocalInspectionToolPhpTests.java
+++ src/test/java/org/mayevskiy/intellij/sonar/SonarLocalInspectionToolPhpTests.java
@@ -1,54 +1,29 @@
 package org.mayevskiy.intellij.sonar;
 
 import com.intellij.psi.PsiFile;
-import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase;
-import org.jetbrains.annotations.Nls;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
 
 /**
- * Author: Oleg Mayevskiy
- * Date: 22.06.13
- * Time: 10:42
+ * @author Oleg Mayevskiy
+ * @author Michail Plushnikov
  */
-public class SonarLocalInspectionToolPhpTests extends LightCodeInsightFixtureTestCase {
+public class SonarLocalInspectionToolPhpTests extends AbstractSonarLocalInspectionTest {
 
-    private SonarLocalInspectionTool sonarLocalInspectionTool = new SonarLocalInspectionTool(null) {
-        @Nls
-        @NotNull
-        @Override
-        public String getDisplayName() {
-            return "sonar";
-        }
+  @Override
+  protected String getBasePath() {
+    return "/src/test/testdata/php";
+  }
 
-        @NotNull
-        @Override
-        public String getStaticDescription() {
-            return "desc";
-        }
+  public void testConvertPsiFileToSonarKeyForPhpFileInRootDir() {
+    PsiFile psiFile = myFixture.configureByFile("Math.php");
+    Collection<String> result = sonarLocalInspectionTool.getSonarKeyCandidatesForPsiFile(psiFile, "sonar:project");
+    assertTrue(result.contains("sonar:project:Math.php"));
+  }
 
-        @NotNull
-        @Override
-        public String getRuleKey() {
-            return "ruleKey";
-        }
-    };
-
-    @Override
-    protected String getTestDataPath() {
-        return "../sonar-intellij-plugin/src/test/testdata/php";
-    }
-
-    public void testConvertPsiFileToSonarKeyForPhpFileInRootDir() {
-        PsiFile psiFile = myFixture.configureByFile("Math.php");
-        Collection<String> result = sonarLocalInspectionTool.getSonarKeyCandidatesForPsiFile(psiFile, "sonar:project");
-        assertTrue(result.contains("sonar:project:Math.php"));
-    }
-
-    public void testConvertPsiFileToSonarKeyForPhpFileInPackageDir() {
-        PsiFile psiFile = myFixture.configureByFile("Bar/Math4.php");
-        Collection<String> result = sonarLocalInspectionTool.getSonarKeyCandidatesForPsiFile(psiFile, "sonar:project");
-        assertTrue(result.contains("sonar:project:Bar/Math4.php"));
-    }
+  public void testConvertPsiFileToSonarKeyForPhpFileInPackageDir() {
+    PsiFile psiFile = myFixture.configureByFile("Bar/Math4.php");
+    Collection<String> result = sonarLocalInspectionTool.getSonarKeyCandidatesForPsiFile(psiFile, "sonar:project");
+    assertTrue(result.contains("sonar:project:Bar/Math4.php"));
+  }
 }
