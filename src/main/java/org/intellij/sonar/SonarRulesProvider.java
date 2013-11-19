c76fb2c5933377a973e1fbda5d6c1441ab0f44b1
Oleg Mayevskiy <mayevskiy.oleg@googlemail.com>
2013-11-20 00:37:02 +0100

    - Improvement: Better detection of new rules - Feature: Mark as fixed locally quick fix

--- src/main/java/org/intellij/sonar/SonarRulesProvider.java
+++ src/main/java/org/intellij/sonar/SonarRulesProvider.java
@@ -9,11 +9,12 @@ import com.intellij.openapi.components.Storage;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.util.xmlb.XmlSerializerUtil;
-import org.jetbrains.annotations.Nullable;
 import org.intellij.sonar.sonarserver.SonarService;
+import org.jetbrains.annotations.Nullable;
 import org.sonar.wsclient.services.Rule;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -23,67 +24,76 @@ import java.util.concurrent.ConcurrentHashMap;
  * Time: 12:42
  */
 @State(
-    name = "SonarRulesProvider",
-    storages = {
-        @Storage(id = "other", file = "$PROJECT_FILE$")
-    }
+        name = "SonarRulesProvider",
+        storages = {
+                @Storage(id = "other", file = "$PROJECT_FILE$")
+        }
 )
 public class SonarRulesProvider implements PersistentStateComponent<SonarRulesProvider> {
 
-  public Map<String, Rule> sonarRulesByRuleKey;
+    public Map<String, Rule> sonarRulesByRuleKey;
 
-  public SonarRulesProvider() {
-    this.sonarRulesByRuleKey = new ConcurrentHashMap<String, Rule>();
-  }
-
-  @SuppressWarnings("UnusedDeclaration")
-  public SonarRulesProvider(Project project) {
-    this();
-  }
-
-  @Nullable
-  @Override
-  public SonarRulesProvider getState() {
-    // workaround NullPointerException of XmlSerializerUtil during serialisation
-    // can be set to null because we don't need this info
-    for (Rule sonarRule : sonarRulesByRuleKey.values()) {
-      sonarRule.setParams(null);
-    }
-    return this;
-  }
-
-  @Override
-  public void loadState(SonarRulesProvider state) {
-    XmlSerializerUtil.copyBean(state, this);
-  }
-
-  public void syncWithSonar(Project project) {
-    final int previousRuleCount = sonarRulesByRuleKey.size();
-
-    clearState();
-    Collection<SonarSettingsBean> allSonarSettingsBeans = SonarSettingsComponent.getSonarSettingsBeans(project);
-    SonarService sonarService = ServiceManager.getService(SonarService.class);
-
-    for (Rule rule : sonarService.getAllRules(allSonarSettingsBeans)) {
-      sonarRulesByRuleKey.put(rule.getKey(), rule);
+    public SonarRulesProvider() {
+        this.sonarRulesByRuleKey = new ConcurrentHashMap<String, Rule>();
     }
 
-    int newRuleCount = sonarRulesByRuleKey.size();
-    if (previousRuleCount != newRuleCount) {
-      // show restart ide dialog
-      final int ret = Messages.showOkCancelDialog("Detected new sonar rules. You have to restart IDE to reload the settings. Restart?",
-          IdeBundle.message("title.restart.needed"), Messages.getQuestionIcon());
-      if (ret == 0) {
-        if (ApplicationManager.getApplication().isRestartCapable()) {
-          ApplicationManager.getApplication().restart();
-        } else {
-          ApplicationManager.getApplication().exit();
+    @SuppressWarnings("UnusedDeclaration")
+    public SonarRulesProvider(Project project) {
+        this();
+    }
+
+    @Nullable
+    @Override
+    public SonarRulesProvider getState() {
+        // workaround NullPointerException of XmlSerializerUtil during serialisation
+        // can be set to null because we don't need this info
+        for (Rule sonarRule : sonarRulesByRuleKey.values()) {
+            sonarRule.setParams(null);
         }
-      }
+        return this;
     }
-  }
 
-  private void clearState() {
-    sonarRulesByRuleKey.clear();
-  }
+    @Override
+    public void loadState(SonarRulesProvider state) {
+        XmlSerializerUtil.copyBean(state, this);
+    }
+
+    public void syncWithSonar(Project project) {
+        Collection<SonarSettingsBean> allSonarSettingsBeans = SonarSettingsComponent.getSonarSettingsBeans(project);
+        SonarService sonarService = ServiceManager.getService(SonarService.class);
+
+        Map<String, Rule> sonarRulesByRuleKeyFromServer = new HashMap<String, Rule>();
+        for (Rule rule : sonarService.getAllRules(allSonarSettingsBeans)) {
+            sonarRulesByRuleKeyFromServer.put(rule.getKey(), rule);
+        }
+
+        if (!equalMaps(sonarRulesByRuleKey, sonarRulesByRuleKeyFromServer)) {
+            sonarRulesByRuleKey.clear();
+            sonarRulesByRuleKey.putAll(sonarRulesByRuleKeyFromServer);
+            showRestartIdeDialog();
+        }
+    }
+
+    private void showRestartIdeDialog() {
+        final int ret = Messages.showOkCancelDialog("Detected new sonar rules. You have to restart IDE to reload the settings. Restart?",
+                IdeBundle.message("title.restart.needed"), Messages.getQuestionIcon());
+        if (ret == 0) {
+            if (ApplicationManager.getApplication().isRestartCapable()) {
+                ApplicationManager.getApplication().restart();
+            } else {
+                ApplicationManager.getApplication().exit();
+            }
+        }
+    }
+
+    private boolean equalMaps(Map<String, Rule> m1, Map<String, Rule> m2) {
+        if (m1.size() != m2.size())
+            return false;
+        for (String key1 : m1.keySet()) {
+            if (!m2.containsKey(key1)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
