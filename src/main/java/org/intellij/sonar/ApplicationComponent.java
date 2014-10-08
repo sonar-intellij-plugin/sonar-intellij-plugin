3735314dd0f72a1c1c958fc6a7a9913f00354ca8
Oleg Majewski <omayevskiy@gmail.com>
2014-10-08 22:22:24 +0200

    fixed ApplicationComponent

---
+++ src/main/java/org/intellij/sonar/ApplicationComponent.java
@@ -0,0 +1,42 @@
+package org.intellij.sonar;
+
+import com.intellij.ide.plugins.IdeaPluginDescriptor;
+import com.intellij.ide.plugins.PluginManager;
+import com.intellij.lang.Language;
+import com.intellij.lang.LanguageExtensionPoint;
+import com.intellij.openapi.extensions.Extensions;
+import com.intellij.openapi.extensions.PluginId;
+import org.intellij.sonar.analysis.SonarExternalAnnotator;
+import org.jetbrains.annotations.NotNull;
+
+public class ApplicationComponent implements com.intellij.openapi.components.ApplicationComponent {
+
+  @Override
+  public void initComponent() {
+    IdeaPluginDescriptor plugin = PluginManager.getPlugin(PluginId.getId("org.mayevskiy.intellij.sonar"));
+    if (plugin != null && plugin.isEnabled()) {
+      registerExternalAnnotatorForAllLanguages(plugin);
+    }
+  }
+
+  private void registerExternalAnnotatorForAllLanguages(IdeaPluginDescriptor plugin) {
+    for (Language language : Language.getRegisteredLanguages()) {
+      LanguageExtensionPoint<SonarExternalAnnotator> extensionPoint = new LanguageExtensionPoint<SonarExternalAnnotator>();
+      extensionPoint.language = language.getID();
+      extensionPoint.implementationClass = SonarExternalAnnotator.class.getName();
+      extensionPoint.setPluginDescriptor(plugin);
+      Extensions.getRootArea().getExtensionPoint("com.intellij.externalAnnotator").registerExtension(extensionPoint);
+    }
+  }
+
+  @Override
+  public void disposeComponent() {
+
+  }
+
+  @NotNull
+  @Override
+  public String getComponentName() {
+    return getClass().getSimpleName();
+  }
+}
