645bfaf3df091b8c859733c21a37ada15254dd84
Oleg Majewski <omayevskiy@gmail.com>
2014-11-08 22:43:54 +0100

    Fixes #76: IDEA hangs and eats all memory

--- src/main/java/org/intellij/sonar/util/SonarComponentToFileMatcher.java
+++ src/main/java/org/intellij/sonar/util/SonarComponentToFileMatcher.java
@@ -1,19 +1,10 @@
 package org.intellij.sonar.util;
 
-import com.google.common.base.Optional;
 import com.intellij.openapi.util.Trinity;
-
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 import static com.intellij.openapi.util.text.StringUtil.isEmptyOrSpaces;
 
 public class SonarComponentToFileMatcher {
-
-  private final static Map<String, String> FILE_KEY_FROM_COMPONENT_CACHE = new ConcurrentHashMap<String, String>();
-  private final static Map<String, String> NORMALIZED_FILE_KEY_CACHE = new ConcurrentHashMap<String, String>();
-  private final static Map<String, String> NORMALIZED_FILE_KEY_FOR_JAVA_CACHE = new ConcurrentHashMap<String, String>();
-  private final static Map<Trinity<String, String, String>, Boolean> MATCH_CACHE = new ConcurrentHashMap<Trinity<String, String, String>, Boolean>();
 
   /**
    *
@@ -24,14 +15,8 @@ public class SonarComponentToFileMatcher {
    */
   public static boolean match(String componentFromSonar, String resourceKeyFromConfiguration, String fullFilePathFromLocalFileSystem) {
     final Trinity<String, String, String> args = new Trinity<String, String, String>(componentFromSonar, resourceKeyFromConfiguration, fullFilePathFromLocalFileSystem);
-    final Optional<Boolean> matchResultFromCache = Optional.fromNullable(MATCH_CACHE.get(args));
-    if (matchResultFromCache.isPresent()) {
-      return matchResultFromCache.get();
-    } else {
-      final boolean matchResult = doMatch(componentFromSonar, resourceKeyFromConfiguration, fullFilePathFromLocalFileSystem);
-      MATCH_CACHE.put(args, matchResult);
-      return matchResult;
-    }
+    final boolean matchResult = doMatch(componentFromSonar, resourceKeyFromConfiguration, fullFilePathFromLocalFileSystem);
+    return matchResult;
   }
 
   @SuppressWarnings("RedundantIfStatement")
@@ -51,18 +36,11 @@ public class SonarComponentToFileMatcher {
 
     // "sonar:project:src/main/java/org/sonar/batch/DefaultSensorContext.java"
     // -> "src/main/java/org/sonar/batch/DefaultSensorContext.java"
-    final Optional<String> fileKeyFromComponentFromCache = Optional.fromNullable(FILE_KEY_FROM_COMPONENT_CACHE.get(componentFromSonar));
-
     final String fileKeyFromComponent;
-    if (fileKeyFromComponentFromCache.isPresent()) {
-      fileKeyFromComponent = fileKeyFromComponentFromCache.get();
+    if (!isEmptyOrSpaces(resourceKeyFromConfiguration)) {
+      fileKeyFromComponent = componentFromSonar.replace(resourceKeyFromConfiguration + ":", "");
     } else {
-      if (!isEmptyOrSpaces(resourceKeyFromConfiguration)) {
-        fileKeyFromComponent = componentFromSonar.replace(resourceKeyFromConfiguration + ":", "");
-      } else {
-        fileKeyFromComponent = componentFromSonar.replaceAll("(?i)(.+:)(.+)", "$2");
-      }
-      FILE_KEY_FROM_COMPONENT_CACHE.put(componentFromSonar, fileKeyFromComponent);
+      fileKeyFromComponent = componentFromSonar.replaceAll("(?i)(.+:)(.+)", "$2");
     }
 
     if (fullFilePathFromLocalFileSystem.endsWith(fileKeyFromComponent)) {
@@ -71,28 +49,18 @@ public class SonarComponentToFileMatcher {
 
     // [root]/VeryBadClassRoot.groovy
     // -> /VeryBadClassRoot.groovy
-    final Optional<String> normalizedFileKeyFromCache = Optional.fromNullable(NORMALIZED_FILE_KEY_CACHE.get(fileKeyFromComponent));
     final String normalizedFileKey;
-    if (normalizedFileKeyFromCache.isPresent()) {
-      normalizedFileKey = normalizedFileKeyFromCache.get();
-    } else {
-      normalizedFileKey = fileKeyFromComponent.replaceAll("\\[.+\\]", "");
-      NORMALIZED_FILE_KEY_CACHE.put(fileKeyFromComponent, normalizedFileKey);
-    }
+    normalizedFileKey = fileKeyFromComponent.replaceAll("\\[.+\\]", "");
+
     if (fullFilePathFromLocalFileSystem.endsWith(normalizedFileKey)) {
       return true;
     }
 
     // .OtherClass
     // -> /OtherClass.java
-    final Optional<String> normalizedFileKeyForJavaFromCache = Optional.fromNullable(NORMALIZED_FILE_KEY_FOR_JAVA_CACHE.get(normalizedFileKey));
     final String normalizedFileKeyForJava;
-    if (normalizedFileKeyForJavaFromCache.isPresent()) {
-      normalizedFileKeyForJava = normalizedFileKeyForJavaFromCache.get();
-    } else {
-      normalizedFileKeyForJava = normalizedFileKey.replaceAll("\\.", "/") + ".java";
-      NORMALIZED_FILE_KEY_FOR_JAVA_CACHE.put(normalizedFileKey, normalizedFileKeyForJava);
-    }
+    normalizedFileKeyForJava = normalizedFileKey.replaceAll("\\.", "/") + ".java";
+
     if (fullFilePathFromLocalFileSystem.endsWith(normalizedFileKeyForJava)) {
       return true;
     }
