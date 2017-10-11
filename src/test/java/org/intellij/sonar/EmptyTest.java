c8e4afd17232905db35d207968701cc32db9bcb4
Oleg Majewski <omayevskiy@gmail.com>
2017-10-11 23:47:22 +0200

    add empty test

---
+++ src/test/java/org/intellij/sonar/EmptyTest.java
@@ -0,0 +1,14 @@
+package org.intellij.sonar;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EmptyTest {
+
+    @Test
+    public void nothing() {
+        // test nothing
+    }
+
+}
