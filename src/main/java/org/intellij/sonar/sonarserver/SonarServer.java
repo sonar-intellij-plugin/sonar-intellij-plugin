493e40d26d482d0d087eeab38ea3d293afcdd971
Jason Elsocht <jelsocht@isabel.eu>
2019-01-04 10:09:33 +0100

    change page size to 499

--- src/main/java/org/intellij/sonar/sonarserver/SonarServer.java
+++ src/main/java/org/intellij/sonar/sonarserver/SonarServer.java
@@ -142,7 +142,7 @@ public class SonarServer {
     public List<Component> getAllProjects(WsClient sonarClient) {
         org.sonarqube.ws.client.component.SearchWsRequest query = new org.sonarqube.ws.client.component.SearchWsRequest()
                 .setQualifiers(singletonList(SonarQualifier.PROJECT.getQualifier()))
-                .setPageSize(10000); //-1 is not allowed, neither int max. The limit is 10000.
+                .setPageSize(499); //-1 is not allowed, neither int max. The limit is 499.
         return sonarClient.components().search(query).getComponentsList();
     }
 
