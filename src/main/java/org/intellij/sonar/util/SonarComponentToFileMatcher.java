8c23839402e3959f6e1aee8d24af38c0cd1721ba
Oleg Majewski <omayevskiy@gmail.com>
2019-01-18 16:36:52 +0100

    refactor SonarComponentToFileMatcher

--- src/main/java/org/intellij/sonar/util/SonarComponentToFileMatcher.java
+++ src/main/java/org/intellij/sonar/util/SonarComponentToFileMatcher.java
@@ -2,81 +2,104 @@ package org.intellij.sonar.util;
 
 import static com.intellij.openapi.util.text.StringUtil.isEmptyOrSpaces;
 
-import com.intellij.openapi.util.Trinity;
-
 public class SonarComponentToFileMatcher {
 
-  /**
-   @param componentFromSonar
-   like sonar:project:src/main/java/org/sonar/batch/DefaultSensorContext.java
-   @param resourceKeyFromConfiguration
-   like sonar:project
-   @param fullFilePathFromLocalFileSystem
-   like /path/to/a.file
+  private boolean match;
+  private boolean processing;
+  private String normalizedFileKey;
+  private String componentFromSonar;
+  private String resourceKeyFromConfiguration;
+  private String fullFilePathFromLocalFileSystem;
+  private String fileKeyFromComponent;
 
-   @return true if the sonar component corresponds to the file, false if not
+  /**
+   * matches sonar resource key to file path
+   * @param componentFromSonar like sonar:project:src/main/java/org/sonar/batch/DefaultSensorContext.java
+   * @param resourceKeyFromConfiguration like sonar:project
+   * @param fullFilePathFromLocalFileSystem like /path/to/a.file
+   * @return true if the sonar component corresponds to the file, false if not
    */
-  public static boolean match(
+  public boolean match(
     String componentFromSonar,
     String resourceKeyFromConfiguration,
     String fullFilePathFromLocalFileSystem
   ) {
-    final Trinity<String,String,String> args = new Trinity<String,String,String>(
-      componentFromSonar,
-      resourceKeyFromConfiguration,
-      fullFilePathFromLocalFileSystem
-    );
-    final boolean matchResult = doMatch(
-      componentFromSonar,
-      resourceKeyFromConfiguration,
-      fullFilePathFromLocalFileSystem
-    );
-    return matchResult;
+    match = false;
+    processing = true;
+    checkNotNullOrEmpty(componentFromSonar, fullFilePathFromLocalFileSystem);
+    this.componentFromSonar = componentFromSonar;
+    this.resourceKeyFromConfiguration = resourceKeyFromConfiguration;
+    this.fullFilePathFromLocalFileSystem = fullFilePathFromLocalFileSystem;
+    if (processing) checkSonarComponentStartsWithResourceKeyFromConfiguration();
+    if (processing) matchByFullPath();
+    if (processing) matchByRootFileName();
+    if (processing) matchByJavaClassFileName();
+    return match;
   }
 
-  @SuppressWarnings("RedundantIfStatement")
-  private static boolean doMatch(
-    String componentFromSonar,
-    String resourceKeyFromConfiguration,
-    String fullFilePathFromLocalFileSystem
-  ) {
-    if (
-      isEmptyOrSpaces(componentFromSonar)
-        || isEmptyOrSpaces(fullFilePathFromLocalFileSystem)
-      ) {
-      return false;
+  private void checkNotNullOrEmpty(String componentFromSonar, String fullFilePathFromLocalFileSystem) {
+    if (isEmptyOrSpaces(componentFromSonar)
+        || isEmptyOrSpaces(fullFilePathFromLocalFileSystem)) {
+      match = false;
+      processing = false;
     }
-    // component = "sonar:project:src/main/java/org/sonar/batch/DefaultSensorContext.java";
-    // resourceKey = "sonar:project"
-    if (!isEmptyOrSpaces(resourceKeyFromConfiguration) && !componentFromSonar.startsWith
-      (resourceKeyFromConfiguration)) {
-      return false;
+  }
+
+  /**
+   * checks if component starts with resource key, like:
+   * component = "sonar:project:src/main/java/org/sonar/batch/DefaultSensorContext.java";
+   * resource key = "sonar:project"
+   */
+  private void checkSonarComponentStartsWithResourceKeyFromConfiguration() {
+    if (!isEmptyOrSpaces(this.resourceKeyFromConfiguration) && !this.componentFromSonar.startsWith
+      (this.resourceKeyFromConfiguration)) {
+      match = false;
+      processing = false;
     }
-    // "sonar:project:src/main/java/org/sonar/batch/DefaultSensorContext.java"
-    // -> "src/main/java/org/sonar/batch/DefaultSensorContext.java"
-    final String fileKeyFromComponent;
+  }
+
+  /**
+   * matches by full path, e.g. if
+   * file key is  "sonar:project:src/main/java/org/sonar/batch/DefaultSensorContext.java"
+   * and full file path on file system is "src/main/java/org/sonar/batch/DefaultSensorContext.java"
+   */
+  private void matchByFullPath() {
     if (!isEmptyOrSpaces(resourceKeyFromConfiguration)) {
-      fileKeyFromComponent = componentFromSonar.replace(resourceKeyFromConfiguration+":","");
+      fileKeyFromComponent = this.componentFromSonar.replace(resourceKeyFromConfiguration +":","");
     } else {
-      fileKeyFromComponent = componentFromSonar.replaceAll("(?i)(.+:)(.+)","$2");
+      fileKeyFromComponent = this.componentFromSonar.replaceAll("(?i)(.+:)(.+)","$2");
     }
-    if (fullFilePathFromLocalFileSystem.endsWith(fileKeyFromComponent)) {
-      return true;
+    if (this.fullFilePathFromLocalFileSystem.endsWith(fileKeyFromComponent)) {
+      match = true;
+      processing = false;
     }
-    // [root]/VeryBadClassRoot.groovy
-    // -> /VeryBadClassRoot.groovy
-    final String normalizedFileKey;
+  }
+
+  /**
+   * matches files in root dir, e.g. if
+   * file key is "[root]/VeryBadClassRoot.groovy"
+   * and full file path on file system is "/VeryBadClassRoot.groovy"
+   */
+  private void matchByRootFileName() {
+
     normalizedFileKey = fileKeyFromComponent.replaceAll("\\[.+\\]","");
-    if (fullFilePathFromLocalFileSystem.endsWith(normalizedFileKey)) {
-      return true;
+    if (this.fullFilePathFromLocalFileSystem.endsWith(normalizedFileKey)) {
+      match = true;
+      processing = false;
     }
-    // .OtherClass
-    // -> /OtherClass.java
+  }
+
+  /**
+   * matches classes without .java suffix, e.g. if
+   * file key is ".OtherClass"
+   * and full file path ends with "/OtherClass.java"
+   */
+  private void matchByJavaClassFileName() {
     final String normalizedFileKeyForJava;
     normalizedFileKeyForJava = normalizedFileKey.replaceAll("\\.","/")+".java";
-    if (fullFilePathFromLocalFileSystem.endsWith(normalizedFileKeyForJava)) {
-      return true;
+    if (this.fullFilePathFromLocalFileSystem.endsWith(normalizedFileKeyForJava)) {
+      match = true;
+      processing = false;
     }
-    return false;
   }
 }
