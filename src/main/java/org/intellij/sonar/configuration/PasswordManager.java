7f2fdd4e898bb2dd1c08988effe29c55e2e8557e
omayevskiy <omvemp198216425>
2020-11-28 22:39:35 +0200

    remove deprecated usage of PasswordSafe.getInstance().storePassword

--- src/main/java/org/intellij/sonar/configuration/PasswordManager.java
+++ src/main/java/org/intellij/sonar/configuration/PasswordManager.java
@@ -1,6 +1,8 @@
 package org.intellij.sonar.configuration;
 
 import com.google.common.base.Throwables;
+import com.intellij.credentialStore.CredentialAttributes;
+import com.intellij.credentialStore.Credentials;
 import com.intellij.ide.passwordSafe.PasswordSafe;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.util.ui.UIUtil;
@@ -9,11 +11,13 @@ public final class PasswordManager {
 
   private static String password;
 
-  public static void storePassword(final String key,final String value) {
+  public static void storePassword(final String user,final String password) {
     UIUtil.invokeAndWaitIfNeeded(
         (Runnable) () -> {
           try {
-            PasswordSafe.getInstance().storePassword(null,PasswordManager.class,key,value);
+            PasswordSafe.getInstance()
+                    .set(new CredentialAttributes(PasswordManager.class.getName(), user)
+                    , new Credentials(user, password));
           } catch (Exception e) {
             Messages.showErrorDialog("Cannot store password\n"+Throwables.getStackTraceAsString(e),"Error");
           }
