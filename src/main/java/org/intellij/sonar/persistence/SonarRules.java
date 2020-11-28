7e2276b3ffda2eb8c909abc9bb6755e2619aa307
omayevskiy <omvemp198216425>
2020-11-28 22:17:13 +0200

    migrate SonarRules projectService

--- src/main/java/org/intellij/sonar/persistence/SonarRules.java
+++ src/main/java/org/intellij/sonar/persistence/SonarRules.java
@@ -1,11 +1,6 @@
 package org.intellij.sonar.persistence;
 
-import java.util.Map;
-import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
-
 import com.intellij.openapi.components.PersistentStateComponent;
-import com.intellij.openapi.components.ProjectComponent;
 import com.intellij.openapi.components.State;
 import com.intellij.openapi.components.Storage;
 import com.intellij.openapi.project.Project;
@@ -14,13 +9,17 @@ import org.intellij.sonar.sonarserver.Rule;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
 @State(
   name = "rules",
   storages = {
     @Storage("sonarRules.xml")
   }
 )
-public class SonarRules implements PersistentStateComponent<SonarRules>, ProjectComponent {
+public class SonarRules implements PersistentStateComponent<SonarRules> {
 
   private Map<String,Rule> sonarRulesByRuleKey = new ConcurrentHashMap<>();
 
@@ -45,27 +44,5 @@ public class SonarRules implements PersistentStateComponent<SonarRules>, Project
 
   public void setSonarRulesByRuleKey(Map<String,Rule> sonarRulesByRuleKey) {
     this.sonarRulesByRuleKey = sonarRulesByRuleKey;
-  }
-
-  @Override
-  public void projectOpened() {
-  }
-
-  @Override
-  public void projectClosed() {
-  }
-
-  @Override
-  public void initComponent() {
-  }
-
-  @Override
-  public void disposeComponent() {
-  }
-
-  @NotNull
-  @Override
-  public String getComponentName() {
-    return "SonarRules";
   }
 }
